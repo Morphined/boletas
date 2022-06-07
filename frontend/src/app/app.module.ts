238a6e61b826bd170f707913ca4bcd7eba19fe55
bercrist5 <cristof140@gmail.com>
2022-06-07 00:03:50 -0500

    se arregla card evento y se adiciona form de creacion de eventos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HeaderCompComponent } from './components/header-comp/header-comp.component';
 import { MedioComponent } from './components/medio/medio.component';
 import { ProximosComponent } from './components/proximos/proximos.component';
@@ -14,10 +15,12 @@ import { LoginCompComponent } from './components/login-comp/login-comp.component
 import { SignupCompComponent } from './components/signup-comp/signup-comp.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CrearEventoComponent } from './components/crear-evento/crear-evento.component';
 
 
 const routes: Routes = [
 { path: 'crearCuenta', component: SignupCompComponent },
+{ path: 'crearEvento', component: CrearEventoComponent },
 { path: 'entrar', component: LoginCompComponent },
 { path: '', component: InicioComponent, pathMatch:'full'},
 { path: '**', redirectTo: '/', pathMatch: 'full' },
@@ -35,9 +38,11 @@ const routes: Routes = [
     LoginCompComponent,
     SignupCompComponent,
     InicioComponent,
-    FooterComponent
+    FooterComponent,
+    CrearEventoComponent
   ],
   imports: [
+    FormsModule,
   BrowserModule, 
   RouterModule.forRoot(routes),
   RouterModule],
